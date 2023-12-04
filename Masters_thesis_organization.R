7b851f77bec5b5b3363b4d2dbacf2f7045c6d714
agreich <alex.reich@alaska.gov>
2023-12-04 14:06:01 -0900

    I sigh at my former, disorganized self. This is the organization for the final submissions, etc, for masters relaxed selection project

---
+++ Masters_thesis_organization.R
@@ -0,0 +1,8 @@
+#####################
+#alex sorts through all of the bullshit
+#that is her masters thesis
+#and organized results
+#in preparation for publication and all that
+
+#12/04/23 (yes, this has been ongoing for far too long)
+###################
